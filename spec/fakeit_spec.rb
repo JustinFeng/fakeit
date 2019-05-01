3c2cb842cacc8d79e5483bb0d9e67b833ca1ba7e
Justin Feng <justin.feng@myob.com>
2019-05-01 17:22:06 +1000

    Added api test for POST, PUT and DELETE

--- spec/fakeit_spec.rb
+++ spec/fakeit_spec.rb
@@ -23,7 +23,69 @@ describe Fakeit do
 
       expect(last_response.headers).to include(
         'Content-Type' => 'application/json',
-        'id' => a_kind_of(String)
+        'Correlation-Id' => a_kind_of(String)
+      )
+    end
+  end
+
+  describe 'POST /' do
+    it 'returns 201' do
+      post '/'
+
+      expect(last_response.created?).to be_truthy
+    end
+
+    it 'returns body' do
+      post '/'
+
+      expect(JSON.parse(last_response.body)).to include('id' => a_kind_of(Integer))
+    end
+
+    it 'returns headers' do
+      post '/'
+
+      expect(last_response.headers).to include(
+        'Content-Type' => 'application/json',
+        'Correlation-Id' => a_kind_of(String)
+      )
+    end
+  end
+
+  describe 'PUT /' do
+    it 'returns 200' do
+      put '/'
+
+      expect(last_response.ok?).to be_truthy
+    end
+
+    it 'returns body' do
+      put '/'
+
+      expect(JSON.parse(last_response.body)).to include('id' => a_kind_of(Integer))
+    end
+
+    it 'returns headers' do
+      put '/'
+
+      expect(last_response.headers).to include(
+        'Content-Type' => 'application/json',
+        'Correlation-Id' => a_kind_of(String)
+      )
+    end
+  end
+
+  describe 'DELETE /' do
+    it 'returns 200' do
+      delete '/'
+
+      expect(last_response.ok?).to be_truthy
+    end
+
+    it 'returns headers' do
+      delete '/'
+
+      expect(last_response.headers).to include(
+        'Correlation-Id' => a_kind_of(String)
       )
     end
   end
