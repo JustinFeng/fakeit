46f69c60d39612fcac838e24df570e2ffa7f1deb
Justin Feng <realfengjia@foxmail.com>
2019-06-16 20:19:37 +1000

    Supported uniqueItems for array example

--- lib/fakeit/openapi/example/array_example.rb
+++ lib/fakeit/openapi/example/array_example.rb
@@ -4,9 +4,40 @@ module Fakeit
       MAX_SIZE = 50
 
       def array_example(use_example)
-        Array.new(Faker::Number.between(minItems || 1, maxItems || MAX_SIZE)) do
-          items.to_example(use_example)
+        size = retries = uniqueItems ? min_array : Faker::Number.between(min_array, max_array)
+
+        generate_items(size, retries, use_example)
+      end
+
+      private
+
+      def generate_items(size, retries, use_example)
+        result = []
+
+        loop do
+          item = items.to_example(use_example)
+
+          if need_retry?(item, result, retries)
+            retries -= 1
+            next
+          end
+
+          break if (result << item).size >= size
         end
+
+        result
+      end
+
+      def need_retry?(item, result, retries)
+        uniqueItems && result.include?(item) && retries.positive?
+      end
+
+      def min_array
+        minItems || 1
+      end
+
+      def max_array
+        maxItems || MAX_SIZE
       end
     end
   end
--- spec/fakeit/openapi/example/array_example_spec.rb
+++ spec/fakeit/openapi/example/array_example_spec.rb
@@ -12,11 +12,37 @@ describe Fakeit::Openapi::Example do
     expect(array.size).to be(1)
   end
 
-  it 'array with minItems and maxItems example' do
+  it 'minItems and maxItems example' do
     expect(Faker::Number).to receive(:between).with(5, 10).and_return(5)
 
     array = schema.properties['array_min_max'].to_example
 
     expect(array.size).to be(5)
+  end
+
+  context 'uniqueItems' do
+    it 'tries to generate minimum items to avoid duplication' do
+      array = schema.properties['array_unique'].to_example
+
+      expect(array.size).to be(2)
+    end
+
+    it 'retries for unique results' do
+      array_schema = schema.properties['array_unique']
+      allow(array_schema.items).to receive(:to_example).and_return(1, 1, 2)
+
+      array = array_schema.to_example
+
+      expect(array).to eq([1, 2])
+    end
+
+    it 'retries for double of the expected size at most' do
+      array_schema = schema.properties['array_unique']
+      allow(array_schema.items).to receive(:to_example).and_return(1, 1, 1, 1)
+
+      array = array_schema.to_example
+
+      expect(array).to eq([1, 1])
+    end
   end
 end
